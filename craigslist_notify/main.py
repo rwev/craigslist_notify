1c95534c3e76c4d58195b3b07c4177322620c3ff
rwev <rwev@rwev.dev>
2020-06-27 22:59:55 -0600

    start with termux

--- craigslist_notify/main.py
+++ craigslist_notify/main.py
@@ -1,12 +1,15 @@
-import urllib
-from collections import defaultdict, namedtuple
+#!/usr/bin/env python3
 
+import subprocess
+import urllib
+from pathlib import Path
+from os.path import expanduser
 import yaml
 import requests
 from bs4 import BeautifulSoup
-from notifyme import NotifyMeMailer
-from pathlib import Path
-from os.path import expanduser
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import List
 
 FILE_BASE = '~/.craigslist_notify_'
 FILE_EXT = '.yaml'
@@ -19,9 +22,11 @@ def load_yaml(path):
     with open(path, 'r') as file:
         return yaml.full_load(file)
 
+
 def save_yaml(path, data):
     with open(path, 'a') as file:
         yaml.dump(data, file)
+
 
 def sanitize_state(state_obj):
     state_obj = dict(state_obj)
@@ -31,23 +36,70 @@ def sanitize_state(state_obj):
             state_obj[region][query] = list(state_obj[region][query])
     return state_obj
 
-def get_current_id_set(region, query):
-    res = requests.get(f'http://{region}.craigslist.org/search/sss?sort=rel&query={urllib.parse.quote(query)}').text
+
+@dataclass
+class Listing:
+    url: str
+    id: int
+    title: str
+    region: str
+    query: str
+
+
+def get_current_listings(r, q) -> List[Listing]:
+    res = requests.get(f'http://{r}.craigslist.org/search/sss?sort=rel&query={urllib.parse.quote(q)}').text
     soup = BeautifulSoup(res, "lxml")
-    listing_items = soup.findAll('a', {'class': 'result-title'})
-    return set([i['data-pid'] for i in listing_items])
+    elements = soup.findAll('a', {'class': 'result-title'})
+    return [Listing(url=e['href'], id=e['data-id'], title=e.text, query=q, region=r) for e in elements]
 
-state = load_yaml(STATE_FILE) or defaultdict(lambda: defaultdict(set))
-config = load_yaml(CONFIG_FILE) or {}
 
-for region in config:
+def termux_notification(listing: Listing):
+    """
+    https://wiki.termux.com/wiki/Termux-notification
+    """
 
-    for query in config[region]:
-        latest_ids = get_current_id_set(region, query)
-        known_ids = set(state[region][query])
-        new_ids = latest_ids - known_ids
-        print (new_ids)
-        known_ids |= new_ids
-        state[region][query] = known_ids
+    print(listing)
+    return
 
-save_yaml(STATE_FILE, sanitize_state(state))
+    subprocess.call([
+        'termux-notification',
+        '--title', f'New \'{listing.query}\' listing in \'{listing.region}\'',
+        '--content', f'{listing.title}',
+        '--action', f'termux-open-url {listing.url}',
+        '--group', 'craigslist_notify'
+    ])
+
+
+def filter_out_known_listings(s, r, q, listings) -> List[Listing]:
+    current_ids = set(map(lambda l: l.id, listings))
+    known_ids = set(s[r][q])
+    new_ids = current_ids - known_ids
+    return list(filter(lambda x: x.id in new_ids, listings))
+
+
+def notify_new_and_update_state(s, r, q):
+    current_listings = get_current_listings(r, q)
+    new_listings = filter_out_known_listings(s, r, q, current_listings)
+
+    list(map(lambda l: termux_notification(l), new_listings))
+
+    s[r][q] = set(s[r][q]) | set(map(lambda l: l.id, new_listings))
+
+
+def main():
+    state = load_yaml(STATE_FILE) or defaultdict(lambda: defaultdict(set))
+    config = load_yaml(CONFIG_FILE) or {}
+
+    for region in config:
+
+        if region not in state:  # handle new query
+            state[region] = defaultdict(set)
+
+        for query in config[region]:
+            notify_new_and_update_state(state, region, query)
+
+    save_yaml(STATE_FILE, sanitize_state(state))
+
+
+if __name__ == '__main__':
+    main()
